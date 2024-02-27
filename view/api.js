67991b1fb6956dfa94ee7161290099eb8ffb1a36
tarou <tarou7684@gmail.com>
2024-02-27 22:50:18 +0900

    update

--- view/api.js
+++ view/api.js
@@ -3,21 +3,25 @@ const app = express();
 const port = 4400;
 const axios = require('axios'); //httpリクエストを行いHTMLを取得、文字列として返す
 const cheerio = require('cheerio'); //HTMLの解析DOM操作を行う
-let tagSrc = [{}];
+let tagSrc = {};
 
 
 
-
+//自分のサイトにアクセス
 app.get("/api/user",(req,res)=>
 {
     let tag;
 
+    //ほしいデータのある場所にアクセス そのまま取得するためにthenでつないで処理
     axios.get(url).then(response =>
     {
     const html = response.data;
     const $loadData = cheerio.load(html);
+    const imgTags = $loadData('img');
     tag=  $loadData('img').attr('src');
-    tagSrc = [{url:tag}];
+    
+    for(let i =0; i < imgTags.length; i++) tagSrc[`url${i}`] = tag;
+    
     res.send(tagSrc);
     console.log(tagSrc);
     }).catch(console.error);
--- view/index.js
+++ view/index.js
@@ -38,4 +38,3 @@ const app = Vue.createApp({
 
 app.mount('#main');
 
-
